3b38359a7b3f274504306454f8d12da3f0d6a88c
gdeignacio <gdeignacio@gmail.com>
2016-09-22 10:36:19 +0200

    Eliminar subprojects

--- sgtsic-libs/sgtsic-libs-utils/src/main/java/es/caib/sgtsic/docx/InvalidOperationException.java
+++
@@ -1,9 +0,0 @@
-package es.caib.sgtsic.docx;
-
-@SuppressWarnings("serial")
-public class InvalidOperationException extends RuntimeException{
-
-	public InvalidOperationException(String message){
-		super(message);
-	}
-}
