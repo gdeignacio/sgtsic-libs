b0270f5942e22fa9f7847ccdbf152891557a765f
gdeignacio <gdeignacio@gmail.com>
2016-10-10 15:13:46 +0200

    Version

--- sgtsic-libs/sgtsic-libs-utils/src/main/java/es/caib/sgtsic/Version.java
+++ sgtsic-libs/sgtsic-libs-utils/src/main/java/es/caib/sgtsic/Version.java
@@ -4,9 +4,7 @@ package es.caib.sgtsic;
 */
 public final class Version {
     public static final String VERSION="0.0.1-SNAPSHOT";
-
-    public static final String BUILDTIME="10/10/2016 10:12";
+    public static final String BUILDTIME="10/10/2016 11:27";
     public static final String SVN_REVISION="-1";
     public static final String JDK_VERSION="1.7.0_80-b15";
-
 }
